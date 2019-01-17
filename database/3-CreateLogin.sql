954fd04ecccc412d50cccb63c960b79033be2431
youngbluder <youngbluder@gmail.com>
2019-01-17 13:23:56 -0500

    New dbase scripts

---
+++ database/3-CreateLogin.sql
@@ -0,0 +1,9 @@
+USE [master]
+GO
+
+/* For security reasons the login is created disabled and with a random password. */
+/****** Object:  Login [WebServerApp]    Script Date: 2019-01-17 11:40:10 AM ******/
+CREATE LOGIN [WebServerApp] WITH PASSWORD=N'MaSiCh2016#', DEFAULT_DATABASE=[MAS500_Test], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
+
+
---
+++ database/4-CreateUser.sql
@@ -0,0 +1,8 @@
+USE [MAS500_Test]
+GO
+
+/****** Object:  User [WebServerApp]    Script Date: 2019-01-17 11:56:22 AM ******/
+CREATE USER [WebServerApp] FOR LOGIN [WebServerApp] WITH DEFAULT_SCHEMA=[dbo]
+GO
+
+
